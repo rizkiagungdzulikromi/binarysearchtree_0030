4be25b5fbba9df92aeca4f70b68532f746d9a46c
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2024-06-14 14:28:56 +0700

    commit 5

--- binarysearchtree_0030.cpp
+++ binarysearchtree_0030.cpp
@@ -45,3 +45,9 @@ public:
 		}
 
 
+		if (element < parent->info) // if the value in the data field of the new node is less than that of the parent
+		{
+			parent->leftchild = NewNode; //make the left child of the parent point to the new node
+		}
+
+		
