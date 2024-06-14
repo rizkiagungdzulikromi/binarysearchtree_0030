9f86ae706070f329b4b5aa677e3b986ae2c4bf58
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2024-06-14 14:27:39 +0700

    commit 3

--- binarysearchtree_0030.cpp
+++ binarysearchtree_0030.cpp
@@ -23,3 +23,18 @@ class BinaryTree
 public:
 	node* ROOT;
 
+	BinaryTree()
+	{
+		ROOT = NULL; //initializing Root to null
+	}
+	void insert(string element) // insert a node in the binary search tree
+	{
+		node* NewNode = new node(element, NULL, NULL); // allocate memory for the new node
+		NewNode->info = element; //Assign value to the data field of the new node
+		NewNode->leftchild = NULL; //Make the left child of the new node point to null
+		NewNode->rightchild = NULL; //Make the right child of the new node point to NULL
+
+		node* parent = NULL;
+		node* currentNode = NULL;
+		search(element, parent, currentNode); //Locate the node which will be the parent of the node to be inserted
+
